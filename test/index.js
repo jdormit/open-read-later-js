855d9bf2c1c98219a8076b60471b0ba2e8ff4055
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-08-01 19:52:40 -0400

    Test #toString

--- test/index.js
+++ test/index.js
@@ -52,4 +52,22 @@ describe('ReadLaterList', function() {
             expect(() => openReadLater.parseReadLaterList(readLaterTextRepeatedUrls)).to.throw();
         });
     });
+    describe('#toString', function() {
+        it('should render a readLaterList to a string', function() {
+            const expectedString =
+`url: https://example.com
+title: Example One
+tags: tag1, tag2
+---
+url: http://example.com
+title: Example Two
+tags: tag2, tag3, tag4
+---
+url: https://jeremydormitzer.com
+title: The best website ever`;
+
+            const readLaterList = openReadLater.parseReadLaterList(readLaterText);
+            expect(readLaterList.toString()).to.equal(expectedString);
+        });
+    });
 });
