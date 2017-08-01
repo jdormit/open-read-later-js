08a4ea3df89a598efc6733fd8792cdaa89140e6a
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-08-01 18:12:06 -0400

    Implement readLaterList instance methods

--- build/open-read-later.js
+++ build/open-read-later.js
@@ -70,7 +70,7 @@ return /******/ (function(modules) { // webpackBootstrap
 /******/ 	__webpack_require__.p = "";
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 2);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -80,110 +80,14 @@ return /******/ (function(modules) { // webpackBootstrap
 "use strict";
 
 Object.defineProperty(exports, "__esModule", { value: true });
-var pipe_1 = __webpack_require__(2);
+var pipe_1 = __webpack_require__(3);
 exports.pipe = pipe_1.default;
-var trace_1 = __webpack_require__(3);
+var trace_1 = __webpack_require__(4);
 exports.trace = trace_1.default;
 
 
 /***/ }),
 /* 1 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-var __assign = (this && this.__assign) || Object.assign || function(t) {
-    for (var s, i = 1, n = arguments.length; i < n; i++) {
-        s = arguments[i];
-        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-            t[p] = s[p];
-    }
-    return t;
-};
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
-            t[p[i]] = s[p[i]];
-    return t;
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-var util_1 = __webpack_require__(0);
-var readLaterList_1 = __webpack_require__(4);
-var validator_1 = __webpack_require__(5);
-var parseColonDelimitedFields = function (fields) {
-    return fields.reduce(function (fieldsObj, field) {
-        return util_1.pipe(function (f) { return /^(.+?):\s?(.+)$/.exec(f); }, function (_a) {
-            var _ = _a[0], key = _a[1], val = _a[2];
-            return (__assign({}, fieldsObj, (_b = {}, _b[key.trim()] = val.trim(), _b)));
-            var _b;
-        })(field);
-    }, {});
-};
-var parseLinkEntry = function (linkEntryText) {
-    return util_1.pipe(function (text) { return text.split('\n'); }, parseColonDelimitedFields, function (_a) {
-        var tags = _a.tags, fields = __rest(_a, ["tags"]);
-        return typeof tags === 'undefined' ? fields : __assign({}, fields, { tags: tags.split(',').map(function (tag) { return tag.trim(); }) });
-    }, readLaterList_1.createLinkEntry)(linkEntryText);
-};
-var parseReadLaterList = function (readLaterText) {
-    return util_1.pipe(function (text) { return text.split('---'); }, function (entries) { return entries.map(function (entry) { return entry.trim(); }); }, function (entryTexts) { return entryTexts.reduce(function (acc, entryText) { return acc.concat([parseLinkEntry(entryText)]); }, []); }, readLaterList_1.createReadLaterList, validator_1.validateReadLaterList)(readLaterText);
-};
-exports.parseReadLaterList = parseReadLaterList;
-
-
-/***/ }),
-/* 2 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-var pipe = function () {
-    var funcs = [];
-    for (var _i = 0; _i < arguments.length; _i++) {
-        funcs[_i] = arguments[_i];
-    }
-    return function (x) { return funcs.reduce(function (y, f) { return f(y); }, x); };
-};
-exports.default = pipe;
-
-
-/***/ }),
-/* 3 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-var trace = function (label) { return function (v) {
-    var prettyV = JSON.stringify(v, null, '\t');
-    console.log(label + ": " + prettyV);
-    return v;
-}; };
-exports.default = trace;
-
-
-/***/ }),
-/* 4 */
-/***/ (function(module, exports, __webpack_require__) {
-
-"use strict";
-
-Object.defineProperty(exports, "__esModule", { value: true });
-var createReadLaterList = function (linkEntries) { return ({ links: linkEntries }); };
-exports.createReadLaterList = createReadLaterList;
-var createLinkEntry = function (_a) {
-    var url = _a.url, title = _a.title, tags = _a.tags;
-    return typeof tags === 'undefined' ? { url: url, title: title } : { url: url, title: title, tags: tags };
-};
-exports.createLinkEntry = createLinkEntry;
-
-
-/***/ }),
-/* 5 */
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
@@ -215,6 +119,129 @@ var validateUniqueUrls = function (readLaterList) {
         return readLaterList;
     }
 };
+
+
+/***/ }),
+/* 2 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+var __assign = (this && this.__assign) || Object.assign || function(t) {
+    for (var s, i = 1, n = arguments.length; i < n; i++) {
+        s = arguments[i];
+        for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+            t[p] = s[p];
+    }
+    return t;
+};
+var __rest = (this && this.__rest) || function (s, e) {
+    var t = {};
+    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+        t[p] = s[p];
+    if (s != null && typeof Object.getOwnPropertySymbols === "function")
+        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) if (e.indexOf(p[i]) < 0)
+            t[p[i]] = s[p[i]];
+    return t;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var util_1 = __webpack_require__(0);
+var readLaterList_1 = __webpack_require__(5);
+var validator_1 = __webpack_require__(1);
+var parseColonDelimitedFields = function (fields) {
+    return fields.reduce(function (fieldsObj, field) {
+        return util_1.pipe(function (f) { return /^(.+?):\s?(.+)$/.exec(f); }, function (_a) {
+            var _ = _a[0], key = _a[1], val = _a[2];
+            return (__assign({}, fieldsObj, (_b = {}, _b[key.trim()] = val.trim(), _b)));
+            var _b;
+        })(field);
+    }, {});
+};
+var parseLinkEntry = function (linkEntryText) {
+    return util_1.pipe(function (text) { return text.split('\n'); }, parseColonDelimitedFields, function (_a) {
+        var tags = _a.tags, fields = __rest(_a, ["tags"]);
+        return typeof tags === 'undefined' ? fields : __assign({}, fields, { tags: tags.split(',').map(function (tag) { return tag.trim(); }) });
+    }, readLaterList_1.createLinkEntry)(linkEntryText);
+};
+var parseReadLaterList = function (readLaterText) {
+    return util_1.pipe(function (text) { return text.split('---'); }, function (entries) { return entries.map(function (entry) { return entry.trim(); }); }, function (entryTexts) { return entryTexts.reduce(function (acc, entryText) { return acc.concat([parseLinkEntry(entryText)]); }, []); }, readLaterList_1.createReadLaterList, validator_1.validateReadLaterList)(readLaterText);
+};
+exports.parseReadLaterList = parseReadLaterList;
+
+
+/***/ }),
+/* 3 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var pipe = function () {
+    var funcs = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        funcs[_i] = arguments[_i];
+    }
+    return function (x) { return funcs.reduce(function (y, f) { return f(y); }, x); };
+};
+exports.default = pipe;
+
+
+/***/ }),
+/* 4 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var trace = function (label) { return function (v) {
+    var prettyV = JSON.stringify(v, null, '\t');
+    console.log(label + ": " + prettyV);
+    return v;
+}; };
+exports.default = trace;
+
+
+/***/ }),
+/* 5 */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+Object.defineProperty(exports, "__esModule", { value: true });
+var util_1 = __webpack_require__(0);
+var validator_1 = __webpack_require__(1);
+var linkEntryToString = function (linkEntry) {
+    return typeof linkEntry.tags === 'undefined'
+        ? "url: " + linkEntry.url + "\ntitle: " + linkEntry.title
+        : "url: " + linkEntry.url + "\ntitle: " + linkEntry.title + "\ntags: " + linkEntry.tags.join(',');
+};
+var createReadLaterList = function (linkEntries) {
+    return ({ links: linkEntries,
+        addLink: function (link) {
+            var _this = this;
+            return util_1.pipe(createLinkEntry, function (linkEntry) { return createReadLaterList(_this.links.concat([linkEntry])); }, validator_1.validateReadLaterList)(link);
+        },
+        removeLink: function (url) {
+            return createReadLaterList(this.links.filter(function (link) { return link.url === url; }));
+        },
+        getLink: function (url) {
+            return this.links.filter(function (link) { return link.url === url; })[0];
+        },
+        updateLink: function (url, newLink) {
+            var _this = this;
+            return util_1.pipe(function (url) { return _this.links.filter(function (link) { return link.url !== url; }); }, function (otherLinks) { return [createLinkEntry(newLink)].concat(otherLinks); }, function (links) { return createReadLaterList(links); }, validator_1.validateReadLaterList)(url);
+        },
+        toString: function () {
+            return this.links.map(linkEntryToString).join('\n---\n');
+        }
+    });
+};
+exports.createReadLaterList = createReadLaterList;
+var createLinkEntry = function (_a) {
+    var url = _a.url, title = _a.title, tags = _a.tags;
+    return typeof tags === 'undefined' ? { url: url, title: title } : { url: url, title: title, tags: tags };
+};
+exports.createLinkEntry = createLinkEntry;
 
 
 /***/ })
--- src/readLaterList/index.ts
+++ src/readLaterList/index.ts
@@ -1,3 +1,8 @@
+import { pipe
+       } from '../util';
+import { validateReadLaterList
+       } from '../validator';
+
 interface LinkEntry
     { url: string
     , title: string
@@ -6,16 +11,48 @@ interface LinkEntry
 
 interface ReadLaterList
     { links: LinkEntry[]
-//    , addLink: (link: LinkEntryConfig) => ReadLaterList
-//    , removeLink: (url: string) => ReadLaterList
-//    , getLink: (url: string) => LinkEntry
-//    , updateLink: (url: string, newLink: LinkEntryConfig) => ReadLaterList
-//    , toString: () => string
+    , addLink: (link: LinkEntryConfig) => ReadLaterList
+    , removeLink: (url: string) => ReadLaterList
+    , getLink: (url: string) => LinkEntry
+    , updateLink: (url: string, newLink: LinkEntryConfig) => ReadLaterList
+    , toString: () => string
     }
 
 type LinkEntryConfig = LinkEntry;
 
-const createReadLaterList = (linkEntries: LinkEntry[]): ReadLaterList => ({links: linkEntries});
+const linkEntryToString = (linkEntry: LinkEntry): string =>
+    typeof linkEntry.tags === 'undefined'
+    ? `url: ${linkEntry.url}
+title: ${linkEntry.title}`
+    : `url: ${linkEntry.url}
+title: ${linkEntry.title}
+tags: ${linkEntry.tags.join(',')}`
+
+const createReadLaterList = (linkEntries: LinkEntry[]): ReadLaterList =>
+    ({ links: linkEntries
+     , addLink: function(link: LinkEntryConfig): ReadLaterList {
+         return pipe( createLinkEntry
+                    , linkEntry => createReadLaterList([...this.links, linkEntry])
+                    , validateReadLaterList
+                    )(link);
+     }
+     , removeLink: function(url: string): ReadLaterList {
+         return createReadLaterList(this.links.filter(link => link.url === url));
+     }
+     , getLink: function(url: string): LinkEntry {
+         return this.links.filter(link => link.url === url)[0];
+     }
+     , updateLink: function(url: string, newLink: LinkEntryConfig): ReadLaterList {
+         return pipe( url => this.links.filter(link => link.url !== url)
+                    , otherLinks => [createLinkEntry(newLink), ...otherLinks]
+                    , links => createReadLaterList(links)
+                    , validateReadLaterList
+                    )(url);
+     }
+     , toString: function(): string {
+         return this.links.map(linkEntryToString).join('\n---\n');
+     }
+     });
 
 const createLinkEntry = ({url, title, tags}: LinkEntryConfig): LinkEntry =>
     typeof tags === 'undefined' ? {url, title} : {url, title, tags};
