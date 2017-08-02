38219b67e564a60fe630152c3a81bf67b3aeffa9
Jeremy Dormitzer <jdormitzer@hubspot.com>
2017-08-01 20:02:23 -0400

    Test #addLink

--- test/index.js
+++ test/index.js
@@ -70,4 +70,43 @@ title: The best website ever`;
             expect(readLaterList.toString()).to.equal(expectedString);
         });
     });
+    describe('#addLink', function() {
+        it('should add a valid link', function() {
+            const readLaterList = openReadLater.parseReadLaterList(readLaterText).addLink({
+                url: 'https://facebook.com',
+                title: 'The Downfall of the Internet',
+                tags: ['evilcorp', 'social media']
+            });
+            expect(readLaterList).to.deep.include({
+                links: [
+                    {
+                        url: 'https://example.com',
+                        title: 'Example One',
+                        tags: [ 'tag1', 'tag2' ]
+                    },
+                    {
+                        url: 'http://example.com',
+                        title: 'Example Two',
+                        tags: [ 'tag2', 'tag3', 'tag4' ]
+                    },
+                    {
+                        url: 'https://jeremydormitzer.com',
+                        title: 'The best website ever'
+                    },
+                    {
+                        url: 'https://facebook.com',
+                        title: 'The Downfall of the Internet',
+                        tags: ['evilcorp', 'social media']
+                    }
+                ]
+            });
+        });
+        it('should throw an error if link is added with a duplicate url', function() {
+            const readLaterList = openReadLater.parseReadLaterList(readLaterText);
+            expect(() => readLaterList.addLink({
+                url: 'https://example.com',
+                title: 'A duplicate URL'
+            })).to.throw();
+        });
+    });
 });
